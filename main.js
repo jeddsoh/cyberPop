0421800d07845ebafebac9976d2dce569c791ce1
Jedd_Soh <jeddsoh@gmail.com>
2023-05-29 07:24:48 -0500

    Update about section

--- main.js
+++ main.js
@@ -5,3 +5,9 @@ menuBtn.addEventListener("click", function () {
   menuBtn.classList.toggle("is-active");
   mobileMenu.classList.toggle("is-active");
 })
+
+window.addEventListener('scroll', function() {
+  const header = document.querySelector('header');
+  const scrolled = window.scrollY > 0;
+  header.classList.toggle('scrolled', scrolled);
+});
