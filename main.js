78e9e346c22e0ad84d88d4715bd52d874ef7dd61
Jedd_Soh <jeddsoh@gmail.com>
2023-07-03 07:00:08 -0500

    Update home page

--- main.js
+++ main.js
@@ -1,65 +1,67 @@
-
-// NEW
 const affirmLink = document.getElementById("affirm-link");
 const yvLink = document.getElementById("yv-link");
 
+const affirm = document.getElementById("affirm");
+const yv = document.getElementById("yv");
+
+
 const brand = document.getElementById("brand");
 const heading = document.getElementById("heading");
-const intro =  document.getElementById("intro");
+const intro = document.getElementById("intro");
 const btnGroup = document.getElementById("btn-group");
 const sketchNote = document.getElementById("sketch-note");
 
-const affirm = document.getElementById("affirm");
 const imgAffirm = document.getElementById("img-affirm");
 const imgAffirmParent = document.getElementById("img-affirm-parent");
 
-affirmLink.addEventListener("mouseenter", function () {
-  imgAffirm.src = "./assets/affirm-cover.png";
+function fadeOut() {
   brand.classList.add("opacity-10");
   heading.classList.add("opacity-10");
   intro.classList.add("opacity-10");
   btnGroup.classList.add("opacity-10");
   sketchNote.classList.add("opacity-10");
-  yvLink.classList.add("opacity-10");
+  imgAffirm.classList.remove("opacity-0");
   imgAffirm.classList.add("opacity-100");
+  imgAffirm.classList.remove("translate-x-72");
   imgAffirm.classList.add("translate-x-0");
-});
+}
 
-affirmLink.addEventListener("mouseleave", function () {
+function fadeIn() {
   brand.classList.remove("opacity-10");
   heading.classList.remove("opacity-10");
   intro.classList.remove("opacity-10");
   btnGroup.classList.remove("opacity-10");
   sketchNote.classList.remove("opacity-10");
-  yvLink.classList.remove("opacity-10");
+  imgAffirm.classList.add("opacity-0");
   imgAffirm.classList.remove("opacity-100");
+  imgAffirm.classList.add("translate-x-72");
   imgAffirm.classList.remove("translate-x-0");
+}
+
+affirmLink.addEventListener("mouseenter", function() {
+  imgAffirm.src = "./assets/affirm-cover.png";
+  yv.classList.add("opacity-10");
+  yv.classList.add("-z-10");
+  fadeOut();
 });
 
-yvLink.addEventListener("mouseenter", function () {
+affirmLink.addEventListener("mouseleave", function() {
+  yv.classList.remove("opacity-10");
+  yv.classList.remove("-z-10");
+fadeIn();
+});
+
+yvLink.addEventListener("mouseenter", function() {
   imgAffirm.src = "./assets/yv-cover.png";
-  yvLink.classList.add("z-20")
-  brand.classList.add("opacity-10");
-  heading.classList.add("opacity-10");
-  intro.classList.add("opacity-10");
-  btnGroup.classList.add("opacity-10");
-  sketchNote.classList.add("opacity-10");
-  affirmLink.classList.add("opacity-10");
+  affirm.classList.add("opacity-10");
   affirm.classList.add("-z-10");
-  imgAffirm.classList.add("opacity-100");
-  imgAffirm.classList.add("translate-x-0");
+  fadeOut();
 });
 
-yvLink.addEventListener("mouseleave", function () {
-  brand.classList.remove("opacity-10");
-  yvLink.classList.remove("z-20")
-  heading.classList.remove("opacity-10");
-  intro.classList.remove("opacity-10");
-  btnGroup.classList.remove("opacity-10");
-  sketchNote.classList.remove("opacity-10");
-  affirmLink.classList.remove("opacity-10");
+yvLink.addEventListener("mouseleave", function() {
+  affirm.classList.remove("opacity-10");
   affirm.classList.remove("-z-10");
-  imgAffirm.classList.remove("opacity-100");
-  imgAffirm.classList.remove("translate-x-0");
-  imgAffirm.src = "./assets/affirm-cover.png";
+  fadeIn();
 });
+
+
