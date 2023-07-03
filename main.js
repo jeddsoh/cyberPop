7937e836fdc812d19365f9eb16d95cddd7d00261
Jedd_Soh <jeddsoh@gmail.com>
2023-07-03 08:36:49 -0500

    Update JS, remove unused variables

--- main.js
+++ main.js
@@ -10,13 +10,11 @@ const intro = document.getElementById("intro");
 const btnGroup = document.getElementById("btn-group");
 const sketchNote = document.getElementById("sketch-note");
 
-const imgAffirm = document.getElementById("img-affirm");
-const imgYV = document.getElementById("img-yv");
+const img = document.getElementById("img-affirm");
 
 const affirmBullet = document.getElementById("affirm-bullet");
 const yvBullet = document.getElementById("yv-bullet");
 
-const imgAffirmParent = document.getElementById("img-affirm-parent");
 
 function fadeOut() {
   brand.classList.add("opacity-10");
@@ -27,10 +25,10 @@ function fadeOut() {
 }
 
 function imgOut() {
-  imgAffirm.classList.remove("opacity-0");
-  imgAffirm.classList.add("opacity-100");
-  imgAffirm.classList.remove("translate-x-72");
-  imgAffirm.classList.add("translate-x-0");
+  img.classList.remove("opacity-0");
+  img.classList.add("opacity-100");
+  img.classList.remove("translate-x-72");
+  img.classList.add("translate-x-0");
 }
 
 function fadeIn() {
@@ -42,19 +40,19 @@ function fadeIn() {
 }
 
 function imgIn() {
-  imgAffirm.classList.add("opacity-0");
-  imgAffirm.classList.remove("opacity-100");
-  imgAffirm.classList.add("translate-x-72");
-  imgAffirm.classList.remove("translate-x-0");
+  img.classList.add("opacity-0");
+  img.classList.remove("opacity-100");
+  img.classList.add("translate-x-72");
+  img.classList.remove("translate-x-0");
 }
 
 affirmLink.addEventListener("mouseenter", function () {
-  imgAffirm.src = "./assets/affirm-cover.png";
+  img.src = "./assets/affirm-cover.png";
   yv.classList.add("opacity-0");
   yv.classList.add("-z-10");
   affirmBullet.classList.add("opacity-0");
   fadeOut();
-  imgAffirm.onload = function () {
+  img.onload = function () {
     imgOut();
   };
 });
@@ -69,13 +67,13 @@ affirmLink.addEventListener("mouseleave", function () {
 });
 
 yvLink.addEventListener("mouseenter", function () {
-  imgAffirm.src = "./assets/yv-cover.png";
+  img.src = "./assets/yv-cover.png";
   affirm.classList.add("opacity-0");
   affirm.classList.add("-z-10");
   yvBullet.classList.add("opacity-0");
 
   fadeOut();
-  imgAffirm.onload = function () {
+  img.onload = function () {
     imgOut();
   };
 });
